890e86b62da1790d0119789778a1deef4007816c
Ilya Zvoznikov <ilya.zvoznikov@gmail.com>
2019-04-05 06:32:48 +0700

    create main app logic

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The app checks status of URLs from file
+
+# How to Install
+
+Python 3 should be already installed. 
+Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+$ pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclop
+edia/pip/pip_virtualenv/) for better isolation.
+
+# Quickstart
+
+The path to the file with URLs is specified when running after the script name
+```bash
+$ python check_sites_health.py urls.txt
+http://google.com
+Server responds with HTTP 200: True
+Domain was paid by more than 1 month: True
+
+http://ya.ru
+Server responds with HTTP 200: True
+Domain was paid by more than 1 month: True
+
+http://devman.org
+Server responds with HTTP 200: True
+Domain was paid by more than 1 month: True
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,83 @@
+import requests
+import whois
+import os
+import datetime
+import argparse
+import sys
+
+
+def get_path_to_urls():
+    parser = argparse.ArgumentParser(
+        description='The app checks status of URLs from file')
+    parser.add_argument('input', help='path to file with URLs list')
+    args = parser.parse_args()
+    return args.input
+
+
 def load_urls4check(path):
-    pass
+    if not os.path.exists(path):
+        return None
+    with open(path, 'r') as file_handler:
+        lines = file_handler.readlines()
+        return [line.strip() for line in lines if line.strip() != '']
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        response = requests.get(url)
+    except (
+            requests.exceptions.ConnectionError,
+            requests.exceptions.ConnectTimeout,
+            requests.exceptions.MissingSchema,
+    ):
+        return False
+
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    try:
+        response = whois.whois(domain_name)
+        expiration_date = response['expiration_date']
+    except:
+        return None
+    if type(expiration_date) is list:
+        return expiration_date[0]
+    else:
+        return expiration_date
+
+
+def is_exp_date_is_more_than(days, exp_date):
+    period = datetime.timedelta(days=days)
+    today = datetime.datetime.today()
+    if exp_date is None:
+        return False
+    else:
+        return (exp_date - today) >= period
+
+
+def print_url_status(url, is_server_responds_with_200, is_exp_date_more):
+    print(url)
+    print('Server responds with HTTP 200: {}'.format(
+        is_server_responds_with_200))
+    print('Domain was paid by more than 1 month: {}'.format(
+        is_exp_date_more))
+    print()
+
 
 if __name__ == '__main__':
-    pass
+    period_in_days = 30
+    path = get_path_to_urls()
+    urls_list = load_urls4check(path)
+    if not urls_list:
+        sys.exit('Incorrect URLs file')
+    for url in urls_list:
+        exp_date = get_domain_expiration_date(url)
+        print_url_status(
+            url,
+            is_server_respond_with_200(url),
+            is_exp_date_is_more_than(period_in_days, exp_date),
+        )
